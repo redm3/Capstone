9bef01cb934b27dfba2a24fbee0777be74dd9be1
marco <macmwz20@gmail.com>
2023-05-01 16:55:04 +1200

    favicon

--- frontend/vite-project/src/components/Stripe/Completion.jsx
+++ frontend/vite-project/src/components/Stripe/Completion.jsx
@@ -1,10 +1,14 @@
+import React, { useEffect } from 'react';
 import './Completion.css';
 
 function Completion(props) {
+  useEffect(() => {
+    localStorage.removeItem('cart');
+  }, []);
+
   return (
     <div className="completion-container">
       <h1>Thank you for your order! 🎉</h1>
-     
     </div>
   );
 }
