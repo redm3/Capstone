bed19e2724de36654622395fe2b138fce70d62ad
marco <56494159+redm3@users.noreply.github.com>
2024-11-15 11:24:39 +1300

    Update Admin.jsx

--- frontend/vite-project/src/pages/Admin/Admin.jsx
+++ frontend/vite-project/src/pages/Admin/Admin.jsx
@@ -8,7 +8,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import TextField from '@mui/material/TextField';
-import './admin.css';
+import '/src/pages/admin/admin.css';
 
 function Admin() {
   const [orders, setOrders] = useState([]);
