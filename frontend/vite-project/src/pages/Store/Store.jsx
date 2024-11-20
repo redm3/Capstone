616e9e1415a96ca4a9ee0ffaad5c3cc8acbbef12
marco <56494159+redm3@users.noreply.github.com>
2024-11-21 10:46:28 +1300

    Update Store.jsx

--- frontend/vite-project/src/pages/Store/Store.jsx
+++ frontend/vite-project/src/pages/Store/Store.jsx
@@ -40,7 +40,7 @@ function Store() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:8000/api/products');
+        const response = await axios.get('https://metro-back-end.vercel.app/api/products');
         setProducts(response.data.data);
       } catch (error) {
         console.error('Error fetching products:', error);
