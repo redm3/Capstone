1588e22a3e47d812fcbcde5fb7bf7e392ea968ca
marco <56494159+redm3@users.noreply.github.com>
2024-11-21 10:49:32 +1300

    Update Login.jsx

--- frontend/vite-project/src/pages/Login.jsx
+++ frontend/vite-project/src/pages/Login.jsx
@@ -60,7 +60,7 @@ export default function Login() {
         let password = passwordProps.value;
     
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/users/login', {
+            const response = await fetch('https://metro-back-end.vercel.app/api/users/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
